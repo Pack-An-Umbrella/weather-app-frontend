b4bce78f52930ae4fc3845f8bd4e6ccf075b1cdf
milkywaynian <marina.faraji1@gmail.com>
2020-03-11 21:17:59 +0000

    Add weather recommendation function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import WeatherComponent from './components/WeatherComponent';
-// import logo from './logo.svg';
-// import './App.css';
 
 class App extends React.Component {
 
   state = {
     weatherParameters: {
             location: "location",
-            feelsLikeTemperature: "9 C",
-            temperature: "12 C",
-            maxUVIndex: "Low Exposure",
+            feelsLikeTemperature: 9,
+            temperature: 12,
+            maxUVIndex: 6,
+            humanReadableValue: 'Low exposure',
             weatherType: "overcast",
+            weatherValue: 8,
             chanceOfRain: "28 %"
             
         }
@@ -21,12 +21,32 @@ class App extends React.Component {
 
 
 
+uvRecommendation = (uvMax) => {
+  if (uvMax >= 3) {
+      return "sunglasses";
+  } else {
+      return "";
+  }
+} 
+
+
+generateRecommendation = (weatherForecast) => {
+  let recommendations = [];
+
+
+  recommendations.push(this.uvRecommendation(weatherForecast.maxUVIndex));
+  recommendations.push("no jacket");
+  recommendations.push("");
+  
+  return recommendations.filter(fil => fil.length > 0).join(", ");
+}
+
 render() {
   return (
     <div className="container">
     <h1>Pack an umbrella</h1>   
     <WeatherComponent allWeather={this.state.weatherParameters}/>
-
+    {this.generateRecommendation(this.state.weatherParameters)}
     </div>
   );
 }
@@ -43,31 +63,6 @@ render() {
 
 
 
-
-
-
-
-
-
-
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
