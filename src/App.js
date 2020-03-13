c87ad663fb55fe4693615288f09d2ea98d9dfe7b
Gauri <gauri.kangai@outlook.com>
2020-03-13 17:38:22 +0000

    Setting up .html and .css

--- src/App.js
+++ src/App.js
@@ -1,73 +1,35 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
+// import typeahead_v2 from './typeahead_v2'
 
 class App extends React.Component {
 
-  state = {
-    weatherParameters: [
-        {
-            location: "location",
-            feelsLikeTemperature: "9 C",
-            temperature: "12 C",
-            maxUVIndex: "Low Exposure",
-            weatherType: "overcast",
-            chanceOfRain: "28 %"
-            
-        }
-    ]
-}
+  // state = {
+  //   weatherParameters: [
+  //     {
+  //       location: "location",
+  //       feelsLikeTemperature: "9 C",
+  //       temperature: "12 C",
+  //       maxUVIndex: "Low Exposure",
+  //       weatherType: "overcast",
+  //       chanceOfRain: "28 %"
+
+  //     }
+  //   ]
+  // }
 
 
 
+  render() {
+    return (
+      <div className="container">
+        <h1>Pack an umbrella - Typeahead</h1>
+      
 
-
-render() {
-  return (
-    <div className="container">
-    <h1>Pack an umbrella</h1>   
-
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ typeahead_v2.js
@@ -0,0 +1,9 @@
+
+class typeahead_v2 extends React.Component {
+
+
+
+}
+
+
+export default typeahead_v2; 
