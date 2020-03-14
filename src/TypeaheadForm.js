968fac8156d71223b2c8959b0ad04c772b29f6bc
Samantha Burke <samanthaburke.uk@gmail.com>
2020-03-14 14:01:05 +0000

    adds fetch, but can't place variables inside of render

--- src/TypeaheadForm.js
+++ src/TypeaheadForm.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 import './App.css';
 
+// Met Office site list, hosted on a static page
+const endpoint = 'https://gist.githubusercontent.com/notwaving/fbccd9e3c668d2da884a0231a44a1bfa/raw/d4544793a8f9d3c377df8916823aa674e5528896/all-locations.json';
+// Create an empty array
+const locations = [];
+
+fetch(endpoint)
+// Grab JSON data
+  .then(blob => blob.json())
+  // Clean up data: if object is missing 'name' or 'unitaryAuthArea', don't break out of code
+  .then(data => data.map(item => {
+    item.name = item.name || ''
+    item.unitaryAuthArea = item.unitaryAuthArea || ''
+    return item
+  }))
+  // Get this data into locations array by spreading into a push method
+  .then(data => locations.push(...data))
+
 class TypeaheadForm extends React.Component {
 
   render() {
@@ -10,7 +27,6 @@ class TypeaheadForm extends React.Component {
       // <div>
       //   <p>Where location search bar will be</p>
       // </div>
-
 
     <form>
         <div className="search-form">
