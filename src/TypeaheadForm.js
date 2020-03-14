3840feafd87f237a19cb0290e81928ec3a6f010b
Samantha Burke <samanthaburke.uk@gmail.com>
2020-03-14 12:46:10 +0000

    form field rendering

--- src/TypeaheadForm.js
+++ src/TypeaheadForm.js
@@ -7,17 +7,17 @@ class TypeaheadForm extends React.Component {
   render() {
     return (
 
-      <div>
-        <p>Where location search bar will be</p>
-      </div>
+      // <div>
+      //   <p>Where location search bar will be</p>
+      // </div>
 
 
-    // <form>
-    //     <div className="search-form">
-    //     <label htmlFor="inputLocation">Enter Location</label>
-    //       <input type="text" className="search" placeholder="Place or area" />
-    //     </div>
-    // </form>
+    <form>
+        <div className="search-form">
+        {/* <label htmlFor="inputLocation">Enter Location</label> */}
+          <input type="text" className="search" placeholder="Place or area" />
+        </div>
+    </form>
 
 );
 }
