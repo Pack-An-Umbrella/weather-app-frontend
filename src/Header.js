13baac21f98214d45d6057b16a66388e16d374e4
Samantha Burke <samanthaburke.uk@gmail.com>
2020-03-17 13:25:24 +0000

    adds font-awesome in index.html

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,8 @@ class Header extends React.Component {
     return (
       <div>
             <header className="text-center">
-            <h1 className="titleheader">PACK AN UMBRELLA</h1>
+            <h1 className="titleheader">PACK AN UMBRELLA 
+            </h1>
           
             </header>
             <h3> Lets go for a <FaBeer />? </h3>
