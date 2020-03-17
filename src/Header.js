9114f7d46182b6cc490f08ee39f7457ed4eaa0e6
Samantha Burke <samanthaburke.uk@gmail.com>
2020-03-17 13:09:49 +0000

    initialises npm icon libraries

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
 
 
 class Header extends React.Component {
@@ -9,7 +10,9 @@ class Header extends React.Component {
       <div>
             <header className="text-center">
             <h1 className="titleheader">PACK AN UMBRELLA</h1>
+          
             </header>
+            <h3> Lets go for a <FaBeer />? </h3>
         </div>
 
     );
